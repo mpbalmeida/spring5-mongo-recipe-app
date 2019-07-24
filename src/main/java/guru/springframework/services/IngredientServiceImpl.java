38787cba8e7c02aed8cad844a2c780da6f4dc290
Marcos Almeida <marcos.almeida@klarna.com>
2019-07-24 17:47:55 +0200

    Migrating to Reactive Repositories

--- src/main/java/guru/springframework/services/IngredientServiceImpl.java
+++ src/main/java/guru/springframework/services/IngredientServiceImpl.java
@@ -47,13 +47,17 @@ public class IngredientServiceImpl implements IngredientService {
         Recipe recipe = recipeOptional.get();
 
         Optional<IngredientCommand> ingredientCommandOptional = recipe.getIngredients().stream()
-            .filter(ingredient -> ingredient.getId().equals(ingredientId))
-            .map(ingredient -> ingredientToIngredientCommand.convert(ingredient)).findFirst();
+                .filter(ingredient -> ingredient.getId().equals(ingredientId))
+                .map(ingredient -> ingredientToIngredientCommand.convert(ingredient)).findFirst();
 
         if (!ingredientCommandOptional.isPresent()) {
             //todo impl error handling
             log.error("Ingredient id not found: " + ingredientId);
         }
+
+        //enhance command object with recipe id
+        IngredientCommand ingredientCommand = ingredientCommandOptional.get();
+        ingredientCommand.setRecipeId(recipe.getId());
 
         return ingredientCommandOptional.get();
     }
@@ -72,18 +76,18 @@ public class IngredientServiceImpl implements IngredientService {
             Recipe recipe = recipeOptional.get();
 
             Optional<Ingredient> ingredientOptional = recipe
-                .getIngredients()
-                .stream()
-                .filter(ingredient -> ingredient.getId().equals(command.getId()))
-                .findFirst();
+                    .getIngredients()
+                    .stream()
+                    .filter(ingredient -> ingredient.getId().equals(command.getId()))
+                    .findFirst();
 
             if (ingredientOptional.isPresent()) {
                 Ingredient ingredientFound = ingredientOptional.get();
                 ingredientFound.setDescription(command.getDescription());
                 ingredientFound.setAmount(command.getAmount());
                 ingredientFound.setUom(unitOfMeasureRepository
-                    .findById(command.getUom().getId())
-                    .orElseThrow(() -> new RuntimeException("UOM NOT FOUND"))); //todo address this
+                        .findById(command.getUom().getId())
+                        .orElseThrow(() -> new RuntimeException("UOM NOT FOUND"))); //todo address this
             } else {
                 //add new Ingredient
                 Ingredient ingredient = ingredientCommandToIngredient.convert(command);
@@ -94,17 +98,17 @@ public class IngredientServiceImpl implements IngredientService {
             Recipe savedRecipe = recipeRepository.save(recipe);
 
             Optional<Ingredient> savedIngredientOptional = savedRecipe.getIngredients().stream()
-                .filter(recipeIngredients -> recipeIngredients.getId().equals(command.getId()))
-                .findFirst();
+                    .filter(recipeIngredients -> recipeIngredients.getId().equals(command.getId()))
+                    .findFirst();
 
             //check by description
             if (!savedIngredientOptional.isPresent()) {
                 //not totally safe... But best guess
                 savedIngredientOptional = savedRecipe.getIngredients().stream()
-                    .filter(recipeIngredients -> recipeIngredients.getDescription().equals(command.getDescription()))
-                    .filter(recipeIngredients -> recipeIngredients.getAmount().equals(command.getAmount()))
-                    .filter(recipeIngredients -> recipeIngredients.getUom().getId().equals(command.getUom().getId()))
-                    .findFirst();
+                        .filter(recipeIngredients -> recipeIngredients.getDescription().equals(command.getDescription()))
+                        .filter(recipeIngredients -> recipeIngredients.getAmount().equals(command.getAmount()))
+                        .filter(recipeIngredients -> recipeIngredients.getUom().getId().equals(command.getUom().getId()))
+                        .findFirst();
             }
 
             //todo check for fail
@@ -129,10 +133,10 @@ public class IngredientServiceImpl implements IngredientService {
             log.debug("found recipe");
 
             Optional<Ingredient> ingredientOptional = recipe
-                .getIngredients()
-                .stream()
-                .filter(ingredient -> ingredient.getId().equals(idToDelete))
-                .findFirst();
+                    .getIngredients()
+                    .stream()
+                    .filter(ingredient -> ingredient.getId().equals(idToDelete))
+                    .findFirst();
 
             if (ingredientOptional.isPresent()) {
                 log.debug("found Ingredient");
---
+++ src/test/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepositoryTest.java
@@ -0,0 +1,52 @@
+package guru.springframework.repositories.reactive;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
+public class UnitOfMeasureReactiveRepositoryTest {
+
+    public static final String DESCRIPTION = "some description";
+    @Autowired
+    UnitOfMeasureReactiveRepository unitOfMeasureReactiveRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        unitOfMeasureReactiveRepository.deleteAll().block();
+    }
+
+    @Test
+    public void save() {
+        UnitOfMeasure unitOfMeasure = new UnitOfMeasure();
+        unitOfMeasure.setDescription(DESCRIPTION);
+
+        unitOfMeasureReactiveRepository.save(unitOfMeasure).block();
+
+        Long count = unitOfMeasureReactiveRepository.count().block();
+
+        assertEquals(Long.valueOf(1L), count);
+    }
+
+    @Test
+    public void findByDescription() {
+        UnitOfMeasure unitOfMeasure = new UnitOfMeasure();
+        unitOfMeasure.setDescription(DESCRIPTION);
+
+        unitOfMeasureReactiveRepository.save(unitOfMeasure).block();
+
+        UnitOfMeasure fetched = unitOfMeasureReactiveRepository.findByDescription(DESCRIPTION).block();
+
+        assertNotNull(fetched);
+        assertNotNull(fetched.getId());
+        assertEquals(DESCRIPTION, fetched.getDescription());
+    }
+}
